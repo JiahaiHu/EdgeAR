b0a362f1818cf31e55eb3f7606401c44618d3e41
Jiahai Hu <johanhu2016@gmail.com>
2022-03-14 20:22:30 +0800

    separate send and receive

--- android/app/src/main/java/com/jiahaihu/edgear/MyTask.java
+++ android/app/src/main/java/com/jiahaihu/edgear/MyTask.java
@@ -21,6 +21,8 @@ public class MyTask extends Thread{
     private String imagePath;
     private int numberOfFrames;
     private int interval;
+    private Socket socket;
+    private String logTimestamp;
 
     public MyTask(String IP, int port, String path, int n, int fps) {
         serverIP = IP;
@@ -28,95 +30,121 @@ public class MyTask extends Thread{
         imagePath = path;
         numberOfFrames = n;
         interval = (int) 1000/fps;
+        logTimestamp = "" + System.currentTimeMillis();
     }
 
     @Override
     public void run() {
+        try {
+            socket = new Socket(serverIP, serverPort);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Thread frameSender = new Thread(() -> {
+            try {
+                String fileDirPath = Environment.getExternalStorageDirectory().toString()  + imagePath;
+                File fileDir = new File(fileDirPath);
+                File[] fileList = fileDir.listFiles();
+                assert fileList != null;
+
+                MyLog myLog = new MyLog("send_" + logTimestamp);
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+                for (int i=0; i<numberOfFrames; i++) {
+                    try {
+                        Thread.sleep(interval);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    int fileLength = (int) fileList[0].length();
+                    Log.d("frameSender", "image file path: " + fileList[0].toString());
+                    Log.d("frameSender", "image file size: " + fileLength);
+
+                    // read image
+                    FileInputStream in =  new FileInputStream(fileList[0].toString());
+                    byte[] buf = new byte[fileLength];
+                    int len = in.read(buf, 0, fileLength);
+                    Log.d("frameSender", "read return: " + len);
+
+                    // send the size of image
+                    byte[] fileLengthByte = bigEndian(fileLength);
+                    out.write(fileLengthByte);
+                    out.flush();
+
+                    // send image to server
+                    long timeStampBegin = System.currentTimeMillis();
+                    String sendTimeBegin = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS").format(new Date());
+                    out.write(buf, 0, fileLength);
+                    out.flush();
+                    long timeStampEnd = System.currentTimeMillis();
+                    String sendTimeEnd = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS").format(new Date());
+                    String timeMsg =  i + "," + sendTimeBegin + "," + sendTimeEnd + "," + timeStampBegin + "," + timeStampEnd;
+                    Log.d("frameSender", "timeMsg: " + timeMsg);
+                    Log.i("frameSender", "index: " + i + " run: upload image success");
+                    myLog.writeFileToLog(timeMsg);
+
+                    in.close();
+                }
+
+                out.close();
+                Log.i("frameSender", "all frames are sent to server");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        frameSender.start();
+
+        // receive result from server
+        Thread resultReceiver = new Thread(() -> {
+            try {
+                MyLog myLog = new MyLog("receive_" + logTimestamp);
+                BufferedReader socket_in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                for (int i=0; i<numberOfFrames; i++) {
+                    Log.d("resultReceiver", "start receiving result from server");
+                    StringBuilder msg = new StringBuilder();
+
+                    while (true) {
+                        Log.i("resultReceiver", "start readline");
+                        String newMsg = socket_in.readLine();
+                        if (newMsg == null) {
+                            break;
+                        }
+                        Log.d("resultReceiver", "newMsg: " + newMsg);
+                        msg.append(newMsg);
+
+                        String end = newMsg.substring(newMsg.length()-2);
+                        Log.d("resultReceiver", "end: " + end);
+                        if (end.equals("**")) {
+                            break;
+                        }
+                    }
+                    Log.d("resultReceiver", "result: " + msg.toString());
+                    String result = msg.substring(0, msg.length()-2);
+
+                    // write result to log file
+                    long timeStamp = System.currentTimeMillis();
+                    String receiveTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS").format(new Date());
+                    String timeMsg =  i + "," + receiveTime + "," + timeStamp;
+                    Log.d("resultReceiver", "timeMsg: " + timeMsg);
+                    myLog.writeFileToLog(timeMsg);
+                }
+
+                socket_in.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        resultReceiver.start();
 
         try {
-            Socket socket = new Socket(serverIP, serverPort);
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-
-//            socket.setKeepAlive(true);
-
-            String fileDirPath = Environment.getExternalStorageDirectory().toString()  + imagePath;
-            File fileDir = new File(fileDirPath);
-
-            File[] fileList = fileDir.listFiles();
-            assert fileList != null;
-
-            Date now = new Date();
-            MyLog myLog = new MyLog(now.toString());
-
-            for (int i=0; i<numberOfFrames; i++) {
-                try {
-                    Thread.sleep(interval);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                // send the size of image
-                int fileLength = (int) fileList[0].length();
-                Log.d("MyTask", "image file path: " + fileList[0].toString());
-                Log.d("MyTask", "image file size: " + fileLength);
-
-
-                // read image
-                FileInputStream in =  new FileInputStream(fileList[0].toString());
-                byte[] buf = new byte[fileLength];
-                int len = in.read(buf, 0, fileLength);
-                Log.d("MyTask", "read return: " + len);
-
-                // send image to server
-                byte[] fileLengthByte = bigEndian(fileLength);
-                out.write(fileLengthByte);
-                out.flush();
-                long timeStampBegin = System.currentTimeMillis();
-                String sendTimeBegin = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS").format(new Date());
-                out.write(buf, 0, fileLength);
-                out.flush();
-                String sendTimeEnd = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS").format(new Date());
-
-//                socket.shutdownOutput();
-                Log.i("MyTask", "index: " + i + " run: upload image success");
-
-                // receive result from server
-                BufferedReader socket_in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                Log.d("MyTask", "start receiving result from server");
-                StringBuilder msg = new StringBuilder();
-
-                while (true) {
-                    Log.i("MyTask", "start readline");
-                    String newMsg = socket_in.readLine();
-                    if (newMsg == null) {
-                        break;
-                    }
-                    Log.d("MyTask", "newMsg: " + newMsg);
-                    msg.append(newMsg);
-
-                    String end = newMsg.substring(newMsg.length()-2);
-                    Log.d("MyTask", "end: " + end);
-                    if (end.equals("**")) {
-                        break;
-                    }
-                }
-                Log.d("MyTask", "result: " + msg.toString());
-                String result = msg.substring(0, msg.length()-2);
-
-                // write result to log file
-                long timeStampEnd = System.currentTimeMillis();
-//                String timeMsg =  i + "," + sendTimeBegin + "," + sendTimeEnd + "," + timeStampBegin + "," + timeStampEnd + "," + (timeStampEnd - timeStampBegin);
-                String timeMsg =  i + "," + (timeStampEnd - timeStampBegin);
-                Log.d("MyTask", "timeMsg: " + timeMsg);
-
-                myLog.writeFileToLog(timeMsg);
-                in.close();
-            }
-
-            out.close();
+            frameSender.join();
+            resultReceiver.join();
             socket.close();
-            Log.i("MyTask", "task is completed");
-
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
